70f073ef36ad9302df7a30c3776111fe03bc51ef
tomorinao <@tomorinao>
2020-05-05 06:12:20 +0900

    ユーザー端末を意識した実装を追加

---
+++ user_client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Timers;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace user_client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Console.WriteLine("host?(httpもしくはhttpsから入力)");
+            var host = Console.ReadLine();
+            while (true)
+            {
+                using (var client = new HttpClient())
+                {
+                    var response = await client.GetAsync(host+"/api/Alert/latest");
+                    Console.WriteLine(await response.Content.ReadAsStringAsync());
+                }
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
